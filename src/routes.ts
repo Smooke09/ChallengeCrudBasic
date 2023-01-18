9855f0e17537eb0d79a9b4267dcfadd19005eaa3
Smooke09 <pedrolucas1014zz@gmail.com>
2023-01-17 22:57:20 -0300

    chore: bug fixed swagger

--- src/routes.ts
+++ src/routes.ts
@@ -5,11 +5,11 @@ const router = Router();
 
 router.get("/stores", StoresController.getStores);
 router.get("/stores/:id", StoresController.getStoreById);
-router.post("/stores", StoresController.createStore);
-router.put("/stores/:id", StoresController.updateStore);
+router.get("/stores/:id/:date/isOpen", StoresController.storeIsOpen);
+router.post("/stores/create", StoresController.createStore);
+router.put("/stores/update/:id", StoresController.updateStore);
 router.put("/stores/:id/business-hours", StoresController.updateBusinessHours);
 router.put("/stores/:id/inactive", StoresController.inactiveStore);
-router.get("/stores/:id/:date/isOpen", StoresController.storeIsOpen);
 router.delete("/stores/:id", StoresController.storeDelete);
 
 export default router;
